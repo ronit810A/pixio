63f9bf375fda82ab089f0526c25e9a2126912db0
ronit810A <ronit810a@gmail.com>
2026-09-09 12:04:11 +0530

    git init

--- js/scrip.js
+++ js/scrip.js
@@ -109,3 +109,41 @@ $('.deal-slider').slick({
     // instead of a settings object
   ]
 });
+
+$('.featured-slider').slick({
+  dots: false,
+  infinite: true,
+  speed: 1000,
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  autoplay: true,
+  autoplaySpeed: 3000,
+  responsive: [
+    {
+      breakpoint: 768,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        infinite: true,
+        dots: false
+      }
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+    // You can unslick at a given breakpoint now by adding:
+    // settings: "unslick"
+    // instead of a settings object
+  ]
+});
